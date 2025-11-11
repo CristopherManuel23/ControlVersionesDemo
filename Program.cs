7ccfda119f17c6630f938e0bec682e7fa2f37a4a
CristopherManuel23 <cristm1538@gmail.com>
2025-11-10 22:14:32 -0400

    Agregada función de login en la rama feature-login

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Sistema de Control de Versiones - Práctica 1");
+        Console.WriteLine("Función de login agregada (simulación).");
+    }
+}
